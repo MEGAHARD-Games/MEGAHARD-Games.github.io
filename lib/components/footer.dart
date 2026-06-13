d01d7a9ac5216f98049abb27875645015579f435
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2026-06-14 00:02:41 +0200

    Updated dependencies

--- lib/components/footer.dart
+++ lib/components/footer.dart
@@ -5,11 +5,8 @@ Footer generateFooter() {
     children: [
       P(
         children: [
-          T("Website last updated on"),
-          Time(
-            datetime: DateTime.now().toIso8601String(),
-            visible: DateTime.now().copyWith(microsecond: 0).toIso8601String().replaceAll("T", " "),
-          ),
+          T("Website last updated on "),
+          Time.now(),
         ],
       ),
     ],
